f6af2940a4cfd1389fdc383c7b6e87b85dc2d999
adammathewvaughn <adammathewvaughn@gmail.com>
2021-08-27 18:41:42 -0500

    still trying to host on gatsbycloud

--- src/pages/index.js
+++ src/pages/index.js
@@ -110,11 +110,11 @@ const links = [
     color: "#8EB814",
   },
   {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
+    text: "Contact Vaughn Software Engineering",
+    url: "https://www.linkedin.com/in/vaughn-software-engineering/",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+      "Contact us today to manage all of your software and web development needs!",
     color: "#663399",
   },
 ]
@@ -125,15 +125,15 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Vaughn Engineering by Gatsby</title>
       <h1 style={headingStyles}>
-        Vaughn Engineering is proud to be a member of the Gatsby community.
+        Vaughn Engineering is proud to be a member of the Gatsby community!
         <br />
-        <span style={headingAccentStyles}>— Va </span>
+        <span style={headingAccentStyles}>Special thanks to StackBlitz. </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
       </h1>
       <p style={paragraphStyles}>
-        GatsbyJS Development is going on the resume immediately.So hip. So swank.
+        Gatsby Development is going on the resume immediately.So hip. So swank.
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
         </span>
@@ -158,7 +158,7 @@ const IndexPage = () => {
               </a>
               {link.badge && (
                 <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
+                  Available Immediately!
                 </span>
               )}
               <p style={descriptionStyle}>{link.description}</p>
@@ -168,7 +168,7 @@ const IndexPage = () => {
       </ul>
       <img
         alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+        src="https://cdn.discordapp.com/attachments/870732357328601129/880954295246274570/pexels-mikhail-nilov-6963744.mp4"
       />
     </main>
   )
