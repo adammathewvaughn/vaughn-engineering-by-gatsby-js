047b8dda6d39324632c42fb8d07b3851d6a70870
adammathewvaughn <adammathewvaughn@gmail.com>
2021-08-27 22:45:09 -0500

    background color added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 // styles
 const pageStyles = {
-  background:" #232129",
+  background:" #FF70A6",
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
