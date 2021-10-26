88b5caaa26032b766384de6071ae9efbe93a292f
adammathewvaughn <adammathewvaughn@gmail.com>
2021-10-25 23:51:43 -0500

    sprucing up the Bruzu partnership

--- src/pages/index.js
+++ src/pages/index.js
@@ -159,9 +159,9 @@ const IndexPage = () => {
         ))}
       </ul>
       <h1 style={headingStyles}><span style={headingAccentStyles}>
-      Vaughn Software Engineering</span> is proud to be a member of the Bruzu community!
+      Vaughn Software Engineering</span> is now a member of the Bruzu community!
       <br />
-      <span style={headingAccentStyles}>At Vaughn Software Engineering we're always learning and evolving, and we're proud to integrate new and innovative technologies to meet the needs<br /> of our customers in an ever-changing landscape.</span>
+      <span style={headingAccentStyles}>At Vaughn Software Engineering we're always learning and evolving, and we're proud to integrate new and innovative technologies like Bruzu to meet the needs<br /> of our customers in an ever-changing landscape.</span>
    
     </h1>
 <span class="bruzu-btn bruzu-btn-theme-light bruzu-btn-size-l">
